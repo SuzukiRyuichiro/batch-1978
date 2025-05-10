cba1f7b8ceaa7497c2f459364b7cc9cf1e33af6b
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-05-10 10:51:37 +0900

    add example

--- hospital/repo_test.rb
+++ hospital/repo_test.rb
@@ -1,11 +1,11 @@
 require_relative 'room_repository'
+require_relative 'patient_repository'
 
-rooms_csv_file = File.join(__dir__, 'rooms.csv')
+room_csv_file_path = File.join(__dir__, './rooms.csv')
+room_repository = RoomRepository.new(room_csv_file_path)
 
-room_repository = RoomRepository.new(rooms_csv_file)
 
-new_room = Room.new({ capacity: 100 })
-
-room_repository.create(new_room)
+patient_csv_file_path = File.join(__dir__, './patients.csv')
+patient_repository = PatientRepository.new(patient_csv_file_path, room_repository)
 
 pp room_repository
