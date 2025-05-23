09c6bfed1e36d59d5334da9d9e3069603cf42255
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-05-24 00:06:36 +0900

    prepped

--- active-record-boilerplate/db/seeds.rb
+++ active-record-boilerplate/db/seeds.rb
@@ -1,1 +1,18 @@
-# This is where you can create initial data for your app.
+Consultation.destroy_all
+Patient.destroy_all
+Intern.destroy_all
+Doctor.destroy_all
+
+tony = Doctor.new(first_name: 'Tony', last_name: 'Stark')
+tony.save
+
+# Let's make peter intern of Tony
+
+peter = Intern.new(first_name: 'Peter', last_name: 'Parker', doctor_id: tony.id)
+peter.save
+
+scooter = Patient.create!(first_name: 'Scoot', last_name: 'Scoot')
+antonio = Patient.create!(first_name: 'Anton', last_name: 'Anton')
+
+Consultation.create!(doctor: tony, patient: scooter)
+Consultation.create!(doctor: tony, patient: antonio)
