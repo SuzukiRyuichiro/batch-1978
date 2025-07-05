e52e112a108b610af40a82f2899a2cb91c4a6616
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-07-05 16:53:40 +0900

    prepped

--- lost_pets_prep/db/seeds.rb
+++ lost_pets_prep/db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Pet.destroy_all
+
+pets = [
+  { name: "Buddy", species: "dog", address: "1-1 Chiyoda, Chiyoda-ku, Tokyo", found_on: Date.today - 2 },
+  { name: "Whiskers", species: "cat", address: "2-2 Shibuya, Shibuya-ku, Tokyo", found_on: Date.today - 5 },
+  { name: "Thumper", species: "rabbit", address: "3-3 Harajuku, Shibuya-ku, Tokyo", found_on: Date.today - 1 },
+  { name: "Slinky", species: "snake", address: "4-4 Akihabara, Chiyoda-ku, Tokyo", found_on: Date.today - 7 },
+  { name: "Shelly", species: "turtle", address: "5-5 Ueno, Taito-ku, Tokyo", found_on: Date.today - 3 },
+  { name: "Max", species: "dog", address: "6-6 Meguro, Meguro-ku, Tokyo", found_on: Date.today - 4 },
+  { name: "Mittens", species: "cat", address: "7-7 Asakusa, Taito-ku, Tokyo", found_on: Date.today - 6 },
+  { name: "Coco", species: "rabbit", address: "8-8 Ebisu, Shibuya-ku, Tokyo", found_on: Date.today - 8 },
+  { name: "Stripes", species: "snake", address: "9-9 Ginza, Chuo-ku, Tokyo", found_on: Date.today - 9 },
+  { name: "Turbo", species: "turtle", address: "10-10 Setagaya, Setagaya-ku, Tokyo", found_on: Date.today - 10 }
+]
+
+pets.each do |pet_attrs|
+  Pet.create!(pet_attrs)
+end
+
+puts "Created #{Pet.count} pets!"
