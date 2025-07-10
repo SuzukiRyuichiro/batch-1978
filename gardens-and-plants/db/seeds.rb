d2d6a3be2146b5080ce399f9e0b8830b85c2cff4
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-07-10 20:42:33 +0800

    seeded plants

--- gardens-and-plants/db/seeds.rb
+++ gardens-and-plants/db/seeds.rb
@@ -1,6 +1,6 @@
 Garden.destroy_all if Rails.env.development?
 
-Garden.create!(
+little = Garden.create!(
   name: "My Little Garden",
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
 )
@@ -8,4 +8,22 @@ Garden.create!(
 Garden.create!(
   name: "My Other Garden",
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+Plant.create!(
+  name: 'Pine tree',
+  image_url: ' https://image.pollinations.ai/prompt/pine+tree',
+  garden: little
+)
+
+Plant.create!(
+  name: 'Cactus',
+  image_url: ' https://image.pollinations.ai/prompt/Cactus',
+  garden: little
+)
+
+Plant.create!(
+  name: 'Hibiscus',
+  image_url: ' https://image.pollinations.ai/prompt/Hibiscus',
+  garden: little
 )
